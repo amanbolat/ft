1a601c610142351e199ee0268131d37e08d5c6a0
Amanbolat <amanbolat@me.com>
2024-01-18 01:06:04 +0100

    update simple example

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -3,71 +3,17 @@ package main
 import (
 	"context"
 	"errors"
-	"log/slog"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/amanbolat/ft"
-	"github.com/hashicorp/go-metrics"
 )
 
+// Running this example will produce an output similar to this:
+//
+// 2024/01/18 01:05:07 INFO action started action=main.Do
+// 2024/01/18 01:05:07 INFO action ended action=main.Do duration=472.583µs error=ERROR
 func main() {
-	ft.EnableMetrics()
-	metricsSink := metrics.NewInmemSink(time.Second*2, time.Minute)
-
-	sig := metrics.DefaultInmemSignal(metricsSink)
-	_, _ = metrics.NewGlobal(metrics.DefaultConfig("example"), metricsSink)
-
-	defer sig.Stop()
-
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     ft.LogLevel(),
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == slog.SourceKey {
-				source := a.Value.Any().(*slog.Source)
-				source.File = ""
-			}
-
-			return a
-		},
-	}))
-	ft.SetLogLevel(slog.Level(1))
-	ft.SetDefaultLogger(logger)
-
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
-	defer stop()
-
-	pid := os.Getpid()
-	proc, err := os.FindProcess(pid)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	go func() {
-		ticker := time.NewTicker(time.Second * 3)
-		for {
-			select {
-			case <-ticker.C:
-				_ = proc.Signal(syscall.SIGUSR1)
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-
-	ticker := time.NewTicker(time.Millisecond * 300)
-LOOP:
-	for {
-		select {
-		case <-ctx.Done():
-			break LOOP
-		case <-ticker.C:
-			_ = Do(ctx)
-		}
-	}
+	ctx := context.Background()
+	_ = Do(ctx)
 
 }
 
