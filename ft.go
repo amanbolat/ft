c217d81d0a1536e7c9225739ddbe30c23c9b64bf
amanbolat <amanbolat@me.com>
2025-01-25 20:33:35 +0100

    refactor: reorganize global config and improve tracing
    This commit:
    - Moves config to global.go with atomic variables
    - Adds duration metric unit options
    - Improves tracing with better error handling
    - Adds support for additional span attributes
    - Introduces clock interface for better testing

--- ft.go
+++ ft.go
@@ -2,11 +2,14 @@ package ft
 
 import (
 	"context"
+	"fmt"
 	"runtime"
 
-	"github.com/hashicorp/go-metrics"
+	"github.com/puzpuzpuz/xsync/v3"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/metric"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 
@@ -14,18 +17,64 @@ import (
 	"time"
 )
 
-func Trace(ctx context.Context, action string) Span {
-	now := time.Now()
+const (
+	instrumentationName           = "github.com/amanbolat/ft"
+	DurationMetricUnitSecond      = "ms"
+	DurationMetricUnitMillisecond = "s"
+)
+
+var int64Counters = xsync.NewMapOf[string, metric.Int64Counter]()
+var durationHistograms = xsync.NewMapOf[string, metric.Float64Histogram]()
+
+type SpanConfig struct {
+	err             *error
+	additionalAttrs []slog.Attr
+}
+
+type Option func(cfg *SpanConfig)
+
+func WithErr(err *error) Option {
+	return func(cfg *SpanConfig) {
+		cfg.err = err
+	}
+}
+
+func WithAttrs(attrs ...slog.Attr) Option {
+	return func(cfg *SpanConfig) {
+		cfg.additionalAttrs = append(cfg.additionalAttrs, attrs...)
+	}
+}
+
+type Span interface {
+	End()
+}
+
+type span struct {
+	ctx             context.Context
+	start           time.Time
+	action          string
+	traceSpan       trace.Span
+	err             *error
+	additionalAttrs []slog.Attr
+}
+
+func Start(ctx context.Context, action string, opts ...Option) (context.Context, Span) {
+	now := (*globalClock.Load()).Now()
+
+	cfg := &SpanConfig{}
+
+	for _, opt := range opts {
+		opt(cfg)
+	}
 
 	if ctx == nil {
 		ctx = context.Background()
 	}
-	var span trace.Span
+	var otelSpan trace.Span
 
-	if tracingEnabled.Load() {
-		ctx, span = otel.Tracer(
+	if globalTracingEnabled.Load() {
+		ctx, otelSpan = otel.Tracer(
 			instrumentationName,
-			trace.WithInstrumentationVersion("git_ref:"+gitCommit),
 			trace.WithSchemaURL(semconv.SchemaURL),
 		).Start(
 			ctx,
@@ -34,63 +83,111 @@ func Trace(ctx context.Context, action string) Span {
 			trace.WithAttributes(
 				attribute.String("action", action),
 			),
+			trace.WithTimestamp(now),
 		)
 	}
 
-	attrs := []slog.Attr{
-		slog.String("action", action),
+	if otelSpan != nil && otelSpan.IsRecording() && globalAppendOtelAttrs.Load() && len(cfg.additionalAttrs) > 0 {
+		otelAttrs := make([]attribute.KeyValue, 0, len(cfg.additionalAttrs))
+		for _, attr := range cfg.additionalAttrs {
+			otelAttrs = append(otelAttrs, mapSlogAttrToOtel(attr))
+		}
+		otelSpan.SetAttributes(otelAttrs...)
 	}
 
-	log(ctx, "action started", logLevel.Level(), now, attrs...)
+	if globalMetricsEnabled.Load() {
+		metricName := action + "_counter"
+		counter, ok := int64Counters.Load(metricName)
 
-	return Span{
-		ctx:       ctx,
-		start:     now,
-		action:    action,
-		traceSpan: span,
-	}
-}
+		if !ok {
+			var err error
+			counter, err = otel.GetMeterProvider().Meter(instrumentationName).Int64Counter(metricName)
+			if err == nil {
+				int64Counters.Store(metricName, counter)
+				ok = true
+			}
+		}
 
-type Span struct {
-	ctx       context.Context
-	start     time.Time
-	action    string
-	traceSpan trace.Span
-	err       *error
-}
-
-func (s Span) WithError(err *error) Span {
-	s.err = err
-
-	return s
-}
-
-func (s Span) Log() {
-	now := time.Now()
-	level := logLevel.Level()
-
-	attrs := []slog.Attr{
-		slog.String("action", s.action),
-		slog.Duration("duration", now.Sub(s.start)),
-	}
-
-	if s.err != nil && *s.err != nil {
-		level = slog.LevelError
-		attrs = append(attrs, slog.Any("error", *s.err))
-
-		if s.traceSpan != nil {
-			s.traceSpan.RecordError(*s.err)
+		if ok {
+			counter.Add(ctx, 1)
 		}
 	}
 
-	if metricsEnabled.Load() {
-		metrics.MeasureSinceWithLabels([]string{s.action}, s.start, nil)
+	attrs := make([]slog.Attr, 1+len(cfg.additionalAttrs))
+	attrs = append(attrs, slog.String("action", action))
+	attrs = append(attrs, cfg.additionalAttrs...)
+
+	log(ctx, "action started", globalLogLevelEndOnSuccess.Level(), now, attrs...)
+
+	return ctx, span{
+		ctx:             ctx,
+		start:           now,
+		action:          action,
+		traceSpan:       otelSpan,
+		err:             cfg.err,
+		additionalAttrs: cfg.additionalAttrs,
+	}
+}
+
+func (s span) End() {
+	now := (*globalClock.Load()).Now()
+	duration := now.Sub(s.start)
+	level := globalLogLevelEndOnSuccess.Level()
+
+	durationAttrKey := "duration_ms"
+	durationAttrVal := durationToMillisecond(duration)
+
+	attrs := make([]slog.Attr, 2+len(s.additionalAttrs))
+	attrs = append(attrs, slog.String("action", s.action), slog.Float64(durationAttrKey, durationAttrVal))
+	attrs = append(attrs, s.additionalAttrs...)
+
+	if s.err != nil && *s.err != nil {
+		level = globalLogLevelEndOnFailure.Level()
+		attrs = append(attrs, slog.Any("error", *s.err))
+
+		if s.traceSpan != nil {
+			s.traceSpan.RecordError(*s.err, trace.WithStackTrace(true))
+			s.traceSpan.SetStatus(codes.Error, (*s.err).Error())
+		}
+	}
+
+	if globalMetricsEnabled.Load() {
+		durationMetricSuffix := "_duration_milliseconds"
+		durationMetricUnit := globalDurationMetricUnit.Load()
+
+		if durationMetricUnit == DurationMetricUnitSecond {
+			durationAttrKey = "duration_s"
+			durationAttrVal = durationToSecond(duration)
+			durationMetricSuffix = "_duration_seconds"
+		}
+
+		metricName := s.action + durationMetricSuffix
+		histogram, ok := durationHistograms.Load(metricName)
+
+		if !ok {
+			var err error
+			histogram, err = otel.GetMeterProvider().
+				Meter(instrumentationName).
+				Float64Histogram(
+					metricName,
+					metric.WithUnit(durationMetricUnit),
+					metric.WithDescription(fmt.Sprintf("[%s] action duration", s.action)),
+				)
+			if err == nil {
+				durationHistograms.Store(metricName, histogram)
+				ok = true
+			}
+		}
+
+		if ok {
+			histogram.Record(s.ctx, duration.Seconds())
+		}
 	}
 
 	log(s.ctx, "action ended", level, now, attrs...)
 
 	if s.traceSpan != nil {
-		s.traceSpan.End()
+		s.traceSpan.End(trace.WithTimestamp(now))
 	}
 }
 
@@ -99,5 +196,37 @@ func log(ctx context.Context, msg string, level slog.Level, now time.Time, attrs
 	runtime.Callers(3, pcs[:])
 	r := slog.NewRecord(now, level, msg, pcs[0])
 	r.AddAttrs(attrs...)
-	_ = DefaultLogger().Handler().Handle(ctx, r)
+	_ = globalLogger.Load().Handler().Handle(ctx, r)
+}
+
+func mapSlogAttrToOtel(v slog.Attr) attribute.KeyValue {
+	key := v.Key
+	value := v.Value
+
+	switch value.Kind() {
+	case slog.KindBool:
+		return attribute.Bool(key, value.Bool())
+	case slog.KindDuration:
+		return attribute.Int64(key, int64(value.Duration()))
+	case slog.KindFloat64:
+		return attribute.Float64(key, value.Float64())
+	case slog.KindInt64:
+		return attribute.Int64(key, value.Int64())
+	case slog.KindString:
+		return attribute.String(key, value.String())
+	case slog.KindTime:
+		return attribute.String(key, value.Time().Format(time.RFC3339))
+	case slog.KindGroup:
+		return attribute.String(key, fmt.Sprintf("%v", value.Group()))
+	default:
+		return attribute.String(key, value.String())
+	}
+}
+
+func durationToMillisecond(d time.Duration) float64 {
+	return float64(d/1000) / 1000
+}
+
+func durationToSecond(d time.Duration) float64 {
+	return float64(d/1000) / 1000000
 }
---
+++ global.go
@@ -0,0 +1,68 @@
+package ft
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/jonboulle/clockwork"
+	"github.com/samber/lo"
+	"go.uber.org/atomic"
+)
+
+var (
+	globalLogger             = atomic.NewPointer(slog.New(slog.NewTextHandler(os.Stdout, nil)))
+	globalTracingEnabled     = atomic.NewBool(false)
+	globalMetricsEnabled     = atomic.NewBool(false)
+	globalAppendOtelAttrs    = atomic.NewBool(false)
+	globalDurationMetricUnit = atomic.NewString(DurationMetricUnitMillisecond)
+	globalClock              = atomic.NewPointer[clockwork.Clock](lo.ToPtr(clockwork.NewRealClock()))
+
+	globalLogLevelEndOnSuccess slog.LevelVar
+	globalLogLevelEndOnFailure slog.LevelVar
+)
+
+func init() {
+	globalLogLevelEndOnSuccess.Set(slog.LevelInfo)
+	globalLogLevelEndOnFailure.Set(slog.LevelError)
+}
+
+func SetDurationMetricUnit(unit string) {
+	switch unit {
+	case DurationMetricUnitMillisecond, DurationMetricUnitSecond:
+		globalDurationMetricUnit.Store(unit)
+	default:
+		globalDurationMetricUnit.Store(DurationMetricUnitMillisecond)
+	}
+}
+
+func SetDefaultLogger(l *slog.Logger) {
+	if l == nil {
+		return
+	}
+
+	globalLogger.Store(l)
+}
+
+func SetLogLevelOnFailure(level slog.Level) {
+	globalLogLevelEndOnFailure.Set(level)
+}
+
+func SetLogLevelOnSuccess(level slog.Level) {
+	globalLogLevelEndOnSuccess.Set(level)
+}
+
+func LogLevelOnFailure() slog.Level {
+	return globalLogLevelEndOnFailure.Level()
+}
+
+func LogLevelOnSuccess() slog.Level {
+	return globalLogLevelEndOnSuccess.Level()
+}
+
+func SetTracingEnabled(v bool) {
+	globalTracingEnabled.Store(v)
+}
+
+func SetMetricsEnabled(v bool) {
+	globalMetricsEnabled.Store(v)
+}
