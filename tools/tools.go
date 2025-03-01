682b643f1395fa7dfdd0beec240e0f1d5d6158a5
amanbolat <amanbolat@me.com>
2025-03-01 01:40:11 +0100

    feat: introduce tools directory

--- tools/tools.go
+++ tools/tools.go
@@ -1,11 +1,7 @@
 //go:build tools
-// +build tools
 
 package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
-
-// This file imports tools that are used in the development process.
-// The build tag ensures they're not included in regular builds.
